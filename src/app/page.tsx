d859bed0b4965d66b2e6f59804faa0c61f6a8984
emir <95132095+emir404@users.noreply.github.com>
2024-07-21 19:27:53 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,14 @@ const Home = () => {
   const router = useRouter();
 
   useEffect(() => {
-    if (process.env.NEXT_PUBLIC_DEV_MODE == "0") {
-      router.push("/coming-soon");
-    }
+      let date = new Date();
+      let openingDate = new Date('2024-08-01');
+
+      if ((date < openingDate) && (process.env.NEXT_PUBLIC_DEV_MODE == '0')) {
+          router.push('/coming-soon');
+      }
   }, []);
+
 
   return (
         <div>
