3aaedc562751a9288129ea1f58f2ca162bf6f2d7
emir <reostraemir@gmail.com>
2024-07-28 18:56:21 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/ui/Header";
-import Footer from "@/components/ui/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
