a8b582a486ad2d5499a1989bdd10365e783ed129
Furkan <96385211+furkanesenn@users.noreply.github.com>
2024-09-05 22:23:37 +0300

    c

--- src/components/ui/sections/AboutUsSection.tsx
+++ src/components/ui/sections/AboutUsSection.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Image from 'next/image';
+
+const Contractors = ["/images/team/Syntra-Media-İhsan-Çarkcı-Contractor.jpeg", "/images/team/Syntra-Media-İhsan-Çarkcı-Contractor.jpeg", "/images/team/Syntra-Media-İhsan-Çarkcı-Contractor.jpeg", "/images/team/Syntra-Media-İhsan-Çarkcı-Contractor.jpeg"]
 
 const AboutUsSection = () => {
     return (
         <div className={"flex w-full overflow-hidden"}>
-            <div className={"flex flex-col gap-24 lg:gap-64 3xl:gap-96 items-center lg:flex-row w-full h-full my-28 mx-8 lg:mx-40"}>
+            <div className={"flex flex-col  lg:gap-24  items-center lg:flex-row w-full h-full my-28 mx-8 lg:mx-40"}>
                 <div className={"flex flex-col justify-center gap-8 w-full h-full"}>
                     <h2 className={"font-semibold text-4xl"}>
                     Our <span className="text-primary-100">dedicated</span> team will get you<br/> top-tier digital marketing results.
@@ -16,24 +19,24 @@ const AboutUsSection = () => {
                     From crafting appealing web designs and optimizing them with SEO, to growing your business on social media, we ensure every aspect of your digital presence  is effectively managed to drive results and support your digital marketing success.
                     </p>
                 </div>
-                <div className={"flex flex-col h-full justify-center items-center gap-8"}>
+                <div className={"flex flex-col w-full h-full justify-center items-center gap-8"}>
                     <div className={"flex w-full gap-8 justify-center"}>
                         <div className={"flex flex-col items-center"}>
-                            <div className={"w-40 h-40 rounded-full bg-white"}></div>
+                            <Image src="/images/team/Syntra-Media-Furkan-Esen-Kurucu-Ortak.jpg" alt="Syntra Media Kurucu Ortağı Furkan Esen"  width="512" height="512" className={"w-40 h-40 rounded-full bg-white"} />
                             <p className={"text-lg mt-3"}>Furkan Esen</p>
-                            <p className={"text-light/70 mt-2"}>CEO</p>
+                            <p className={"text-light/70 mt-2"}>Co-founder</p>
                         </div>
                         <div className={"flex flex-col items-center"}>
-                            <div className={"w-40 h-40 rounded-full bg-white"}></div>
+                        <Image src="/images/team/Syntra-Media-Emir-Ayaz-Kurucu-Ortak.jpg" alt="Syntra Media Kurucu Ortağı Emir Ayaz"  width="512" height="512" className={"w-40 h-40 rounded-full bg-white"} />
                             <p className={"text-lg mt-3"}>Emir Ayaz</p>
-                            <p className={"text-light/70 mt-2"}>CEO</p>
+                            <p className={"text-light/70 mt-2"}>Co-founder</p>
                         </div>
                     </div>
                     <div className={"flex flex-col items-center"}>
                         <div className={"flex -space-x-16"}>
                             {
-                                Array.from({length: 4}).map((_, index) => (
-                                    <div key={index} className={"w-32 h-32 rounded-full bg-white border border-neutral-900"}></div>
+                                Contractors.map((_, index) => (
+                                    <Image src={Contractors[index]} alt={`Syntra Media Kontraktörü ${index}`} key={index} width="512" height="512" className={"w-32 h-32 rounded-full bg-white border border-neutral-900"} />
                                 ))
                             }
                         </div>
