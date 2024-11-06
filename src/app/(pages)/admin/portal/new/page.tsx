cd4916792ffe7bf16654ea2434078f75f72b8c19
emir <reostraemir@gmail.com>
2024-11-06 19:46:59 +0300

    fix

--- src/app/(pages)/admin/portal/new/page.tsx
+++ src/app/(pages)/admin/portal/new/page.tsx
@@ -10,7 +10,6 @@ export default async function NewProject() {
       id: user.id,
       firstName: user.firstName,
       lastName: user.lastName,
-      email: user.emailAddresses?.[0]?.emailAddress || null,
       avatar: user.imageUrl,
     }))
   ));
